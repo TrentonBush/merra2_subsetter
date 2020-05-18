44d90bc5643a2780c0b8e2fb7b8c668389180850
t-desktop <tpbush@gmail.com>
2020-05-18 15:00:12 -0700

    add multiple collections

--- async_downloader_test_script.py
+++ async_downloader_test_script.py
@@ -5,18 +5,33 @@ import async_downloader
 from pathlib import Path
 
 urls = merra_urls.url_generator(
-    time_interval=(datetime(2020, 3, 30), datetime(2020, 4, 2)),
+    time_interval=(datetime(2014, 1, 1), datetime(2019, 1, 1)),
     lat_interval=(26, 37),
     lon_interval=(-107, -93),
     collections=[
         {
             "collection": "tavg1_2d_slv_Nx",
             "short_name": "M2T1NXSLV",
-            "fields": ["PS", "T10M", "U50M", "V50M"],
-        }
+            "fields": ["PS", "TS", "T10M", "U50M", "V50M"],
+        },
+        {
+            "collection": "inst1_2d_asm_Nx",
+            "short_name": "M2I1NXASM",
+            "fields": ["U50M", "V50M"],
+        },
+        {
+            "collection": "tavg1_2d_flx_Nx",
+            "short_name": "M2T1NXFLX",
+            "fields": ["PRECTOTCORR", "RHOA", "RISFC", "Z0M"],
+        },
+        {
+            "collection": "tavg1_2d_lnd_Nx",
+            "short_name": "M2T1NXLND",
+            "fields": ["GHLAND"],
+        },
     ],
 )
 
-dl = async_downloader.AsyncDownloader(Path('./data/'))
+dl = async_downloader.AsyncDownloader(Path("~/c/data/merra_texas"))
 
 asyncio.run(dl.download(urls))
