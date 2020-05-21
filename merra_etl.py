992522ec855acfdffa9082f732c02af717d71595
t-desktop <tpbush@gmail.com>
2020-05-21 16:56:20 -0700

    draft merra nc4 to parquet pipeline

--- merra_etl.py
+++ merra_etl.py
@@ -1,7 +1,7 @@
 import xarray as xr
 import numpy as np
 from pathlib import Path
-from typing import Optional, Union
+from typing import Optional, Union, Sequence
 
 
 def binary_round(
@@ -10,7 +10,7 @@ def binary_round(
     binary_bits: Optional[int] = None,
     decimal_digits: Optional[int] = None,
 ) -> Union[xr.Dataset, xr.DataArray, np.ndarray]:
-    """Round an array of floats to a specific number of bits. The number of bits can be specified directly, with binary_bits, or converted from decimal_digits. The decimal_digits conversion will always round with at least as much precision as the decimal_digits.
+    """Round an array of floats to a specific number of bits. The number of bits can be specified directly, with binary_bits, or calculated from decimal_digits. The decimal_digits conversion rounds with at least as much precision as the decimal_digits.
     Note: input negative bits to round to the left of the floating point.
     Motivation is to enable higher compression by removing false precision from data.
     
@@ -57,12 +57,12 @@ def binary_round(
     return (ds * multiplier).round() * divisor
 
 
-def chunk_transformations(ds: xr.Dataset) -> None:
-    """Column transformations for MERRA-2 data.
+def chunk_transformations(ds: xr.Dataset) -> xr.Dataset:
+    """Column transformations for MERRA-2 data. Note that modifications are performed IN PLACE, but I can't drop variables in place so have to return a new xr.Dataset without those variables.
     * Change temperature units from K to degrees C
     * Convert wind vector components to magnitude and direction (degrees in [0,360] from North, positive going clockwise)
     * Log10 transform precipitation
-    * Remove unused columns
+    * Remove unused variables
 
     Parameters
     ----------
@@ -71,8 +71,8 @@ def chunk_transformations(ds: xr.Dataset) -> None:
 
     Returns
     -------
-    None
-        Modifications are IN PLACE
+    xr.Dataset
+        Modifications are IN PLACE but must return a dataset in order to drop variables
     """
     ds.update(ds[["TS", "T10M"]] - 273.15)  # convert units K -> C
     ds["WS50M"] = np.sqrt(np.square(ds["V50M"]) + np.square(ds["U50M"]))
@@ -104,7 +104,7 @@ def reduce_precision(ds: xr.Dataset, fp16=False) -> None:
     if fp16:
         f16 = ["GHLAND", "RHOA", "PRECTOTCORR", "RISFC", "TS", "T10M"]
         ds.update(ds[f16].astype(np.float16).astype(np.float32))
-        for dec, cols in {1: ["WDIR50M"], 3: ["WS50M"]}:
+        for dec, cols in {1: ["WDIR50M"], 3: ["WS50M"]}.items():
             ds.update(binary_round(ds[cols], decimal_digits=dec))
     else:
         prec = {
@@ -118,27 +118,106 @@ def reduce_precision(ds: xr.Dataset, fp16=False) -> None:
 
 def make_divisions(
     ds: xr.Dataset,
+    *,
     points_per_partition: Optional[int] = None,
     mb_per_partition: Optional[int] = None,
 ) -> list:
-    """Calculate partition divisions for converting an xr.Dataset to a dask.DataFrame given either points per partition or megabytes per partition. Form is list(0, step size, 2* step size, ... , last index value) """
+    """Calculate partition divisions for converting an xr.Dataset to a dask.DataFrame given either points per partition or megabytes per partition. Either option will produce partitions split on grid point boundaries, to ensure load order wont matter in the future.
+    
+    Parameters
+    ----------
+    ds : xr.Dataset
+        dataset with all component arrays having the same time dimension
+    points_per_partition : Optional[int], optional
+        Number of grid points to include in each dask.DataFrame partition, by default None
+    mb_per_partition : Optional[int], optional
+        Max size in megabytes of each dask.DataFrame partition, by default None
+
+    Returns
+    -------
+    list
+        Form is list(0, 1 * step size, 2 * step size, ... , last index value)
+
+    Raises
+    ------
+    ValueError
+        When both points_per_partition and mb_per_partition are specified (conflict)
+    ValueError
+        When neither points_per_partition or mb_per_partition are specified
+    """
     time = ds.time.size
     total_points = len(ds.lat) * len(ds.lon)
     if points_per_partition:
         if mb_per_partition:
             raise ValueError(
-                f"Choose only one of points_ or mb_ per_partition. Given {points_per_partition} and {mb_per_partition}, respectively"
+                f"Choose only one of points_per_partition or mb_ per_partition. Given {points_per_partition} and {mb_per_partition}, respectively"
             )
         step_size = time * points_per_partition
         steps = total_points // points_per_partition
         return [step_size * i for i in range(steps)] + [time * total_points - 1]
 
     elif mb_per_partition:
-        mb_per_point = ds.nbytes / total_points / 2 ** 20
-        points = mb_per_partition // mb_per_point
+        mb_per_point = ds.nbytes / total_points / (2 ** 20)
+        points = int(mb_per_partition // mb_per_point)
         return make_divisions(ds, points_per_partition=points)
 
     else:
         raise ValueError(
-            f"Must choose one of points_ or mb_ per_partition. Given {points_per_partition} and {mb_per_partition}, respectively"
+            f"Must choose one of points_per_partition or mb_ per_partition. Given {points_per_partition} and {mb_per_partition}, respectively"
         )
+
+
+def merra_nc4_to_parquet(
+    files_in: Sequence[Path],
+    dir_out: Path,
+    precision_reduction: Optional[str] = "round",
+    max_megabytes_per_file: int = 100,
+) -> None:
+    """API to convert a folder of daily netCDF MERRA-2 data to columnar parquet files. This method only works for data that fits in memory.
+
+    Parameters
+    ----------
+    files_in : Sequence[Path]
+        sequence of file paths, such as Path(<directory>).glob(<PATTERN>)
+    dir_out : Path
+        directory where parquet files will be written
+    precision_reduction : Optional[str], optional
+        One of None, 'round', or 'fp16', by default 'round'
+    max_megabytes_per_file : int, optional
+        max uncompressed size of each output parquet file, in megabytes. Actual files will be smaller due to compression. By default 100
+
+    Returns
+    -------
+    None
+    """
+    ds = xr.open_mfdataset(
+        files_in,
+        combine="by_coords",
+        concat_dim="time",
+        data_vars="minimal",
+        coords="minimal",
+        compat="no_conflicts",
+    )
+
+    total_points = len(ds.lat) * len(ds.lon)
+    mb_per_point = ds.nbytes / total_points / 2 ** 20
+    points_per_chunk = max_megabytes_per_file / mb_per_point
+    if points_per_chunk < total_points:
+        # this binary tiling algorithm is pretty conservative on chunk size, but should be good enough
+        log_n_chunks = np.ceil(
+            np.log2(total_points / points_per_chunk)
+        )  # nearest power of 2, rounded up
+        log_lat_chunks = log_n_chunks // 2
+        log_lon_chunks = log_n_chunks - log_lat_chunks
+        lat_step = np.ceil(len(ds.lat) / (2 ** log_lat_chunks))
+        lon_step = np.ceil(len(ds.lon) / (2 ** log_lon_chunks))
+        ds = ds.chunk(chunks={"lat": lat_step, "lon": lon_step, "time": None})
+    ds = chunk_transformations(ds)
+    if precision_reduction is not None:
+        fp16 = precision_reduction == "fp16"
+        reduce_precision(ds, fp16=fp16)
+    divisions = make_divisions(ds, mb_per_partition=max_megabytes_per_file)
+    ddf = ds.to_dask_dataframe()
+    ddf.repartition(divisions=divisions).to_parquet(
+        dir_out, compression="snappy", write_index=False
+    )
